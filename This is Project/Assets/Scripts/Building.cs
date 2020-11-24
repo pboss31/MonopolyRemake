7d3063e6049511f62d1997527dfd1b367ffe899e
jeszer <73539417+jeszer@users.noreply.github.com>
2020-11-24 13:38:42 +0700

    main

---
+++ This is Project/Assets/Scripts/Building.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Building : MonoBehaviour, ITakeDamage
+{
+
+    [SerializeField] private int _owner;
+    [SerializeField] private int _price;
+    [SerializeField] private int _hp;
+
+    public int Owner { get => _owner; set => _owner = value; }
+    public int Price { get => _price; set => _price = value; }
+    public int Hp { get => _hp; set => _hp = value; }
+
+    StateManager theStateManager;
+    PlayerData[] thePlayerData;
+
+    public void Start()
+    {
+        theStateManager = GameObject.FindObjectOfType<StateManager>();
+        thePlayerData = GameObject.FindObjectsOfType<PlayerData>().OrderBy(m => m.transform.GetSiblingIndex()).ToArray();
+    }
+
+    public void Update()
+    {
+        if (_owner == 0)
+        {
+            transform.GetChild(1).GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        }
+        else if (_owner == 1)
+        {
+            transform.GetChild(1).GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+        }
+        else if (_owner == 2)
+        {
+            transform.GetChild(1).GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+        }
+        else
+        {
+            transform.GetChild(1).GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+        }
+    }
+
+    public void TakeDamage()
+    {
+        //decrease house hp
+        _hp -= thePlayerData[theStateManager.CurrentPlayerId].Atk;
+    }
+
+    public void BuyHouse()
+    {
+        //show house, decrease player coin, set owner
+        transform.GetChild(1).gameObject.SetActive(true);
+        thePlayerData[theStateManager.CurrentPlayerId].Coin -= _price;
+        _owner = theStateManager.CurrentPlayerId;
+    }
+}
---
+++ This is Project/Assets/Scripts/PlayerPiece.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class PlayerPiece : MonoBehaviour {
+
+    StateManager theStateManager;
+    PlayerAction[] thePlayerAction;
+
+    // Use this for initialization
+    void Start ()
+    {
+        theStateManager = GameObject.FindObjectOfType<StateManager>();
+        targetPosition = this.transform.position;
+        thePlayerAction = GameObject.FindObjectsOfType<PlayerAction>().OrderBy(m => m.transform.GetSiblingIndex()).ToArray();
+    }
+
+    [SerializeField] private Tile currTile;
+    public Tile CurrTile { get => currTile; set => currTile = value; }
+
+    public int PlayerId;
+
+    Tile[] moveQueue;
+    int moveQueueIndex;
+    int spacesToMove;
+
+    bool isAnimating = false;
+
+    Vector3 targetPosition;
+    Vector3 velocity = Vector3.zero;
+    float smoothTime = 0.15f;
+    float smoothTimeVertical = 0.1f;
+    float smoothDistance = 0.01f;
+    float smoothHeight = 0.5f; //0.5f
+
+    void Update()
+    {
+        if (isAnimating == false) { return; }
+
+        if (Vector3.Distance( new Vector3(this.transform.position.x, targetPosition.y, this.transform.position.z), targetPosition) < smoothDistance)
+        {
+            // we have reached that target. how's our height?
+
+            //going down
+            if (moveQueueIndex == moveQueue.Length && this.transform.position.y > smoothDistance)
+            {
+                this.transform.position = Vector3.SmoothDamp(this.transform.position, new Vector3(this.transform.position.x, 0, this.transform.position.z),ref velocity, smoothTimeVertical);
+            }
+            else
+            {
+                //right pos,  right height -> advance the cube
+                AdvanceMoveQueue();
+            }
+        }
+        
+        else if (this.transform.position.y < (smoothHeight - smoothDistance))
+        {
+            //going up
+            this.transform.position = Vector3.SmoothDamp(this.transform.position, new Vector3(this.transform.position.x, smoothHeight, this.transform.position.z), ref velocity, smoothTimeVertical);
+        }
+        
+        else
+        {
+            //move
+            this.transform.position = Vector3.SmoothDamp(this.transform.position, new Vector3(targetPosition.x, smoothHeight, targetPosition.z), ref velocity, smoothTime);
+        }
+        
+    }
+
+    void AdvanceMoveQueue()
+    {
+        // moveQueue != null 
+        if (moveQueueIndex < moveQueue.Length)
+        {
+            SetNewTargetPosition(moveQueue[moveQueueIndex].transform.position);
+            moveQueueIndex++;
+        }
+        //movement = null, so we are done animating
+        else
+        {         
+            Debug.Log("DONE advancing!!");
+            this.isAnimating = false;
+
+            //doing some actions when done animating
+            thePlayerAction[theStateManager.CurrentPlayerId].OnCall(); 
+        }
+    }
+
+    void SetNewTargetPosition(Vector3 pos)
+    {
+        targetPosition = pos;
+        //velocity = Vector3.zero;
+    }
+
+    //happen right after DiceRoller is done
+    public void MovePlayer()
+    {
+        //is this the correct player
+        if (theStateManager.CurrentPlayerId != PlayerId) { return; }
+
+        //have we rolled?
+        if (theStateManager.IsDoneRolling == false) { return; }
+
+        //set spacestomove
+        spacesToMove = theStateManager.DiceTotal;
+
+        //where should we end up
+        moveQueue = new Tile[spacesToMove];
+        Tile finalTile = currTile;
+
+        for (int i = 0; i < spacesToMove; i++)
+        {
+            finalTile = finalTile.NextTile[0];
+            moveQueue[i] = finalTile;
+        }
+
+        moveQueueIndex = 0;
+        currTile = finalTile;
+        this.isAnimating = true;
+
+
+        Debug.Log("Player: " + theStateManager.CurrentPlayerId + " | CurrTiles = " + currTile + " | " + currTile.Id + " " + currTile.Name + " " + currTile.GetComponent<Building>().Price);
+    }
+
+}
