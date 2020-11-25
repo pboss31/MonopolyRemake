3e6cdac469f0c456d13efb46fba7952f477c5b9e
SSpetch7 <64187488+SSpetch7@users.noreply.github.com>
2020-11-25 23:44:28 +0700

    sendDB

---
+++ This is Project/Assets/Scripts/SendTODB.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class SendToDB : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Send("variable"));
+    }
+
+    IEnumerator Send(string date)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("date", date);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/monopoly/SendMatch.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("www.downloadHandler.text");
+            }
+        }
+    }
+}
